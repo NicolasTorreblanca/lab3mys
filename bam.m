934cae67282e9c3684f79a8f2d9ae8c062d258a9
NicolasTorreblanca <nicolas.torreblanca@usach.cl>
2022-11-27 18:56:50 -0300

    comentarios parte 1

--- bam.m
+++ bam.m
@@ -1,3 +1,9 @@
+% Funcion bam
+% Entradas : flotante x flotante x flotante x flotante x flotante x flotante 
+% Salidas  : Matriz x Matriz x Matriz x Matriz
+% Funcionamiento : Calcula las matrices de un modelo de estados
+%                  dado determinados parametros de entrada.
+
 function [A,B,C,D] = bam(a,b,c,d,e,f)
     A = [c/b -a/b ; d/e f/e];
     B = [a/b ; 0];
--- mab.m
+++ mab.m
@@ -1,3 +1,9 @@
+% Funcion mab
+% Entradas : Matriz x Matriz x Matriz x Matriz
+% Salidas  : Funcion de transferencia
+% Funcionamiento : Calcula una función de transferencia a partir de las
+%                  4 matrices de modelo de estados.
+
 function H = mab(A,B,C,D)
     s = tf ('s');
     H = C * (inv(s*eye(2) - A)) * B + D;
