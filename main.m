febd6e7e6fae0748f552356b98211a26dc705c82
Rodrigo <rodrigo.escobar.z@usach.cl>
2022-11-27 22:52:09 -0300

    finalizacion entrega

--- main.m
+++ main.m
@@ -95,9 +95,13 @@ u = 100* sin(t2/4);
 u(u<0) = 0.;
 u_t = lsim(H2, u, t2);
 
+figure(2)
 plot(t1,transpose(escalon(:,1)), t1, transpose(impulso(:,1)), t2, transpose(u_t(:,1)))
 title('Comparación de respuestas con distintas entradas');
 legend("Respuesta a escalón","Respuesta a impulso", "Respuesta a u(t) = 100* sin(t/4)");
+xlim([0 40])
+xlabel("Tiempo(s)")
+ylabel("Amplitud")
 
 % ################################################
 
@@ -119,7 +123,7 @@ M_z2 = c2d(M2, T2,'zoh');
 M_z3 = c2d(M2, T3,'zoh');
 
 % Se comparan los resultados con la discretización zoh
-figure(5)
+figure(3)
 step(M_z1, M_z2, M_z3)
 title('Discretizaciones zero order hold con tiempo de muestreo distinto');
 legend("T = 0.001","T = 0.1", "T = 2");
@@ -131,7 +135,7 @@ M_f2 = c2d(M2, T2,'foh');
 M_f3 = c2d(M2, T3,'foh');
 
 % Se comparan los resultados con la discretización foh
-figure(6)
+figure(4)
 step(M_f1, M_f2, M_f3)
 title('Discretizaciones first order hold con tiempo de muestreo distinto');
 legend("T = 0.001","T = 0.1", "T = 2");
